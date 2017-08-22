25fba04e038251a5c372a5c5d3eea49ae7da2779
Luis Ricardo Sanchez <luissanchez0305@gmail.com>
2017-08-22 22:52:57 +0000

    cambio de prueba response test

--- www/js/app.js
+++ www/js/app.js
@@ -92,6 +92,7 @@ angular.module('starter', ['ionic', 'config', 'languages', 'starter.controllers'
 .config(function($stateProvider, $urlRouterProvider, $httpProvider, $ionicConfigProvider, $translateProvider, contentfulClientProvider, myAppConfig) {
 
   $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+  $httpProvider.defaults.timeout = 100000;
   contentfulClientProvider.setSpaceId(myAppConfig.contentfulSpaceId); 
   contentfulClientProvider.setAccessToken(myAppConfig.contentfulAccessToken);
   $ionicConfigProvider.views.maxCache(0);
